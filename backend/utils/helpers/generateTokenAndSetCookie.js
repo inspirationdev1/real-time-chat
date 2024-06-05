e837b55cde221e6e651bd90926b58afc894f131a
maroofirfan <maroofirfan@a2kitsolutions.com>
2024-06-05 19:39:38 +0530

    Worked on Backend

---
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenSetCookie = (userId, res) => {
+  const token = jwt.sign({userId},process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  })
+  res.cookie("jwt", token, {
+    httpOnly: true, //more secure
+    maxAge: 1 * 24 * 60 * 1000, //1 day
+    sameSite:"strict", //CSRF (security vurnability)
+  })
+
+  return token;
+}
+
+export default generateTokenSetCookie;
+
